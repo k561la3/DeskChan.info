66c67b1d9231884d1810766a25b4a87bdb228351
Oleh <k561la3@meta.ua>
2017-06-12 16:55:42 +0300

    added snaps & drag borders

--- deskchan.js
+++ deskchan.js
@@ -1,18 +1,3 @@
-/**
- * Created by User on 12.06.2017.
- */
-
 $(document).ready(function () {
-
-
-
-
-
-
-    //$html("\<div id=\"draggable\" class=\"ui-widget-content\"\> \<img src=\"shocked.png\"/> </div>");
-$(draggable).draggable();
+$("#draggable").draggable({ snap: ".dragborder", containment: ".dragborder" });
 });
-
-
-
-
