5d171c5f38f1377ba4ae451d48772c255df7534f
Oleh <k561la3@meta.ua>
2017-06-12 14:47:38 +0300

    minor fixes of deskchan.info

---
+++ deskchan.js
@@ -0,0 +1,18 @@
+/**
+ * Created by User on 12.06.2017.
+ */
+
+$(document).ready(function () {
+
+
+
+
+
+
+    //$html("\<div id=\"draggable\" class=\"ui-widget-content\"\> \<img src=\"shocked.png\"/> </div>");
+$(draggable).draggable();
+});
+
+
+
+
